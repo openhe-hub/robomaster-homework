df2631a222f7feb5b5eb3ea33ab6a996b83a78c7
openhe <625529334@qq.com>
2022-10-08 01:25:29 +0800

    10-8-upd: ex1

--- homework1/src/ex1/ex1.sh
+++ homework1/src/ex1/ex1.sh
@@ -1,3 +1,12 @@
-for file in ${ls \home} ; do
-    echo $file
+cd $1
+cnt=0
+start=`date +%s`
+file_list=$(find -name "*.c" -o -name "*.h" | xargs grep -l "hello")
+for file in $file_list ; do
+    printf "%s\n" $file
+    cnt=$(($cnt+ 1))
 done
+printf "total found: %d\n" $cnt
+end=`date +%s`
+printf "total time=%f seconds\n" $(($end-$start))
+
