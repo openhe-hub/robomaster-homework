cc7e5c5ea0ecb0a6655e0512392a6dda7185b025
openhe <625529334@qq.com>
2022-10-19 20:57:30 +0800

    homework2 finish

---
+++ homework2/cmake-build-debug/CMakeFiles/gl-test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gl-test.dir/test/test.cpp.o"
+  "gl-test"
+  "gl-test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gl-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ homework2/cmake-build-debug/CMakeFiles/gl-test.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for gl-test.
+# This may be replaced when dependencies are built.
